7976dca15e81a4b6ca7820749e15f6b522795686
git config --global user.name 'vasiliy.shevchuk <git config --global user.email 'vasiliy.shevchuk@chisw.com>
2024-01-17 15:18:36 +0200

    fix

--- src/components/Reveal.tsx
+++ src/components/Reveal.tsx
@@ -6,7 +6,7 @@ interface RevealProps {
     width?: "fit-content" | '100%'
 }
 
-export const Reveal = ({ children, width = "fit-content" }: RevealProps) => {
+export const Reveal = ({ children }: RevealProps) => {
     const ref = useRef(null)
     const isInView = useInView(ref)
 
@@ -20,7 +20,6 @@ export const Reveal = ({ children, width = "fit-content" }: RevealProps) => {
     return (
         <div ref={ref} style={{ overflow: 'hidden' }}>
             <motion.div
-                style={{ width }}
                 variants={{
                     hidden: { opacity: 0, y: 75 },
                     visible: { opacity: 1, y: 0 },
